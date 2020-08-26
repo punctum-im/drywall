036d654d9c04434cf388b5e5991c9af2fd9d3e6d
knuxify <knuxify@gmail.com>
2020-08-26 20:57:56 +0200

    tests: Add basic object generation test
    This commit adds a new file, test_objects.py, which contains object-related
    tests. Currently, the only function available is generate_objects, which
    generates objects for every available object type.

---
+++ tests/test_objects.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+This file contains tests related to objects. It can be included from other
+tests to extend their functionality (in particular, the generate_objects
+function can be used to quickly generate objects and IDs for testing
+purposes).
+"""
+# This is used to import the actual modules:
+import sys
+sys.path.insert(1, './src/')
+
+import objects
+import db_dummy as db
+
+def generate_objects():
+	"""
+	This function generates objects of all types and returns a list
+	containing the dict with objects and a dict with their IDs, both
+	sorted by object type.
+	"""
+	created_objects = {}
+	created_ids = {}
+	for object in ['instance', 'account', 'conference', 'conference_user', 'invite', 'role', 'channel', 'message', 'attachment']:
+		# Reset the created_dict and set our object class. The latter is used to
+		# get values like required keys, key types, etc. to know how to fill
+		# an object dict.
+		created_dict = {"object_type": object}
+		object_class = objects.get_object_class_by_type(object)
+
+		# First, we go over all the required keys for the given object:
+		for key in object_class.required_keys:
+			key_type = object_class.key_types[key]
+			# Depending on the key type, we're going to set them differently.
+			if key_type == "string":
+				if key == "channel_type":
+					created_dict[key] = "text"
+					created_dict["parent_conference"] = created_ids["conference"]
+				elif key == "attachment_type":
+					created_dict[key] = "quote"
+					created_dict["quoted_message"] = created_ids["message"]
+				else:
+					created_dict[key] = key + "_string"
+			elif key_type == "number":
+				created_dict[key] = 1
+			elif key_type == "boolean":
+				created_dict[key] = "false"
+			elif key_type == "permission_map":
+				created_dict[key] = "11111"
+			elif key_type == "id":
+				created_dict[key] = created_ids[object_class.id_key_types[key]]
+			elif key_type == "id_list":
+				# While we could just add the same key twice, this might end up
+				# breaking once ID list key deduplication is implemented. A way to
+				# work around this would be to generate multiple keys for a key
+				# type, but this would require a larger chunk of code to handle
+				# giving out keys (remember we have to account for singular IDs
+				# with the same IDs too). Currently this is not the case however,
+				# so we can ignore it for now.
+				created_dict[key] = [created_ids[object_class.id_key_types[key]]]
+			else:
+				raise TypeError("Invalid key type")
+
+		# Finally, we create an object from the dict and add it to our
+		# created_objects and created_ids dicts, for use in later tests.
+		object_generated_from_dict = objects.make_object_from_dict(created_dict)
+		created_objects[object] = object_generated_from_dict.__dict__
+		db.add_object(object_generated_from_dict)
+		created_ids[object] = object_generated_from_dict.__dict__["id"]
+
+	return [created_objects, created_ids]
+
+# Commands to execute when the test is ran manually.
+if __name__ == "__main__":
+	# We don't need to do any exception handling here, just crashing
+	# in the original functions is enough because in this case we need
+	# to raise the exception anyways.
+	generate_objects()
