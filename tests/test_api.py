576f061fef4b492b2f47bca2c64920eb885602cd
knuxify <knuxify@gmail.com>
2020-09-08 20:47:42 +0200

    tests: overhaul of pregenerated variables + more coverage
    The pytest fixture previously used for pregenerating objects has been
    replaced with a class. This is to prevent problems in the account by-name
    methods as they break with multiple accounts with the same names, and
    the pytest fixture regenerated the objects for every function. Now a
    single class generates the objects beforehand, avoiding such errors.

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,55 +15,51 @@ def client():
     with drywall.app.test_client() as client:
         yield client
 
-@pytest.fixture
-def pregenerate_objects():
-	"""
-	Uses the generate_objects function to generate some objects. Returns
-	the results of said command.
-	"""
-	return generate_objects()
+class PregeneratedObjects:
+	"""Contains pregenerated objects and their IDs."""
+	pregenerated_objects = generate_objects()
+	dicts = pregenerated_objects[0]
+	ids = pregenerated_objects[1]
 
-def test_api_id(pregenerate_objects, client):
+def test_api_id(client):
 	"""Test the ID-related APIs."""
-	pregenerated_object_dict = pregenerate_objects[0]
-	pregenerated_object_ids = pregenerate_objects[1]
 
 	# GET /api/v1/instance
 	assert client.get('/api/v1/instance').status == "200 OK"
 
 	# GET /api/v1/id/<id>
-	account_get = client.get('/api/v1/id/' + pregenerated_object_ids['account'])
+	account_get = client.get('/api/v1/id/' + PregeneratedObjects.ids['account'])
 	assert account_get.status == "200 OK"
-	assert account_get.get_json() == pregenerated_object_dict['account']
+	assert account_get.get_json() == PregeneratedObjects.dicts['account']
 	nonexistent_get = client.get('/api/v1/id/fakeid')
 	assert nonexistent_get.status == "404 NOT FOUND"
 
 	# PATCH /api/v1/id/<id>
 	message_patch_dict = {"content": "customcontent"}
-	message_patch = client.patch('/api/v1/id/' + pregenerated_object_ids['message'], json=message_patch_dict)
-	message_get = client.get('/api/v1/id/' + pregenerated_object_ids['message'])
+	message_patch = client.patch('/api/v1/id/' + PregeneratedObjects.ids['message'], json=message_patch_dict)
+	message_get = client.get('/api/v1/id/' + PregeneratedObjects.ids['message'])
 	assert message_patch.status == "200 OK"
-	assert message_get.get_json() != pregenerated_object_dict['message']
+	assert message_get.get_json() != PregeneratedObjects.dicts['message']
 	assert message_get.get_json()['content'] == "customcontent"
 	nonexistent_patch = client.patch('/api/v1/id/fakeid', json=message_patch_dict)
 	assert nonexistent_patch.status == "404 NOT FOUND"
-	patch_with_no_json = client.patch('/api/v1/id/' + pregenerated_object_ids['message'])
+	patch_with_no_json = client.patch('/api/v1/id/' + PregeneratedObjects.ids['message'])
 	assert patch_with_no_json.status == "400 BAD REQUEST"
 
 	# POST /api/v1/id
-	message_json = pregenerated_object_dict['message']
+	message_json = PregeneratedObjects.dicts['message']
 	object_post = client.post('/api/v1/id', json=message_json)
 	object_post_json = object_post.get_json()
 	assert object_post.status == "200 OK"
 	assert 'error' not in object_post_json
-	assert object_post_json['id'] != pregenerated_object_dict['message']['id']
+	assert object_post_json['id'] != PregeneratedObjects.dicts['message']['id']
 	assert object_post_json['type'] == "object"
 	assert object_post_json['object_type'] == "message"
 	object_post_no_json = client.post('/api/v1/id')
 	assert object_post_no_json.status == "400 BAD REQUEST"
 
 	# GET /api/v1/id/<id>/type
-	attachment_get = client.get('/api/v1/id/' + pregenerated_object_ids['attachment'] + '/type')
+	attachment_get = client.get('/api/v1/id/' + PregeneratedObjects.ids['attachment'] + '/type')
 	attachment_json = attachment_get.get_json()
 	assert attachment_get.status == "200 OK"
 	assert "quoted_message" not in attachment_json
@@ -76,7 +72,7 @@ def test_api_id(pregenerate_objects, client):
 
 	# GET /api/v1/stash/request
 	object_ids = list()
-	for object_id in pregenerated_object_ids.values():
+	for object_id in PregeneratedObjects.ids.values():
 		object_ids.append(object_id)
 	stash_request = {"id_list": object_ids}
 	stash_get = client.get('/api/v1/stash/request', json=stash_request)
@@ -87,32 +83,82 @@ def test_api_id(pregenerate_objects, client):
 	stash_get_no_json = client.get('/api/v1/stash/request')
 	assert stash_get_no_json.status == "400 BAD REQUEST"
 
-def test_api_accounts(pregenerate_objects, client):
+def test_api_accounts(client):
 	"""Tests account-related APIs."""
-	pregenerated_object_dict = pregenerate_objects[0]
-	pregenerated_object_ids = pregenerate_objects[1]
-	account_id = pregenerated_object_ids['account']
-	account_name = pregenerated_object_dict['account']['username']
+	account_id = PregeneratedObjects.ids['account']
+	account_name = PregeneratedObjects.dicts['account']['username']
+	account_dict = PregeneratedObjects.dicts['account']
 
 	# GET /api/v1/accounts/<id>
 	assert client.get('/api/v1/accounts/' + account_id).status == "200 OK"
-	assert client.get('/api/v1/accounts/' + pregenerated_object_ids['message']).status == "400 BAD REQUEST"
+	assert client.get('/api/v1/accounts/' + account_id).get_json() == account_dict
+	assert client.get('/api/v1/accounts/' + PregeneratedObjects.ids['message']).status == "400 BAD REQUEST"
 	assert client.get('/api/v1/accounts/fakeid').status == "404 NOT FOUND"
+
+	# PATCH /api/v1/accounts/<id>
+	account_id_patch = client.patch('/api/v1/accounts/' + account_id, json={"bio": "custombio"})
+	account_id_patch_json = account_id_patch.get_json()
+	account_patch_dict_target = {}
+	for key, value in account_dict.items():
+		account_patch_dict_target[key] = value
+	account_patch_dict_target['bio'] = "custombio"
+	assert account_id_patch.status == "200 OK"
+	assert account_id_patch_json == account_patch_dict_target
+	assert client.patch('/api/v1/accounts/' + PregeneratedObjects.ids['message'], json={"bio": "custombio"}).status == "400 BAD REQUEST"
+	assert client.patch('/api/v1/accounts/' + account_id).status == "400 BAD REQUEST"
+	assert client.patch('/api/v1/accounts/fakeid').status == "404 NOT FOUND"
 
 	# TODO: POST /api/v1/accounts/<bot_id>/invite
 	# This function is not correctly implemented yet, and does not return
 	# anything if everything goes right.
 
 	# GET /api/v1/accounts/by-name/<name>
-	assert client.get('/api/v1/accounts/by-name/' + account_name).status == "200 OK"
+	account_name_get = client.get('/api/v1/accounts/by-name/' + account_name)
+	assert account_name_get.status == "200 OK"
+	assert account_name_get.get_json() == account_patch_dict_target
 	assert client.get('/api/v1/accounts/by-name/fakename').status == "404 NOT FOUND"
 
 	# PATCH /api/v1/accounts/by-name/<name>
-	account_patch = client.patch('/api/v1/accounts/by-name/' + account_name, json={"bio": "custombio"})
+	account_patch = client.patch('/api/v1/accounts/by-name/' + account_name, json={"bio": "custombio2"})
 	account_json = account_patch.get_json()
+	account_patch_dict_target['bio'] = "custombio2"
 	assert account_patch.status == "200 OK"
-	assert account_json['bio'] == "custombio"
-	account_patch_fake_name = client.patch('/api/v1/account/by-name/fakename', json={"bio": "custombio"})
+	assert account_patch.get_json() == account_patch_dict_target
+	account_patch_fake_name = client.patch('/api/v1/account/by-name/fakename', json={"bio": "custombio2"})
 	assert account_patch_fake_name.status == "404 NOT FOUND"
 	account_patch_no_json = client.patch('/api/v1/accounts/by-name/' + account_name)
 	assert account_patch_no_json.status == "400 BAD REQUEST"
+
+def test_api_messages(client):
+	"""Tests message-related APIs."""
+	message_id = PregeneratedObjects.ids['message']
+	message_dict = PregeneratedObjects.dicts['message']
+	message_dict['content'] = "customcontent"
+
+	# GET /api/v1/messages/<id>
+	message_get = client.get('/api/v1/messages/' + message_id)
+	assert message_get.get_json() == message_dict
+	assert message_get.status == "200 OK"
+	assert client.get('/api/v1/messages/' + PregeneratedObjects.ids['account']).status == "400 BAD REQUEST"
+	assert client.get('/api/v1/messages/fakeid').status == "404 NOT FOUND"
+
+	# PATCH /api/v1/messages/<id>
+	message_id_patch = client.patch('/api/v1/messages/' + message_id, json={"content": "customcontent2"})
+	message_id_patch_json = message_id_patch.get_json()
+	assert message_id_patch.status == "200 OK"
+	message_patch_dict_target = {}
+	for key, value in message_dict.items():
+		message_patch_dict_target[key] = value
+	message_patch_dict_target['content'] = "customcontent2"
+	assert message_id_patch_json == message_patch_dict_target
+	assert client.patch('/api/v1/messages/' + PregeneratedObjects.ids['account'], json={"content": "customcontent2"}).status == "400 BAD REQUEST"
+	assert client.patch('/api/v1/messages/' + message_id).status == "400 BAD REQUEST"
+	assert client.patch('/api/v1/messages/fakeid').status == "404 NOT FOUND"
+
+	# POST /api/v1/messages
+	message_post = client.post('/api/v1/messages', json=message_dict)
+	assert message_post.status == "200 OK"
+	message_post_id = message_post.get_json()['id']
+	message_post_target_dict = message_post.get_json()
+	message_post_target_dict['id'] = message_post_id
+	assert message_post.get_json() == message_post_target_dict
