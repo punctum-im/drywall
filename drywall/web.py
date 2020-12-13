3a299d3bd90529a6fc3bf4d733fd557d7c503920
knuxify <knuxify@gmail.com>
2020-12-13 22:32:25 +0100

    web: further updates for the settings page

--- drywall/web.py
+++ drywall/web.py
@@ -5,9 +5,15 @@ pages. Authentication pages are defined in the auth module.
 """
 from drywall import db
 from drywall import app
+from drywall import objects
 from drywall import settings
 
-from flask import redirect, render_template, session, url_for
+from flask import redirect, render_template, request, session, url_for
+from email_validator import validate_email, EmailNotValidError
+
+def update_user():
+	"""Updates the user dict stored in session storage"""
+	session["user_dict"] = db.get_object_as_dict_by_id(session["user_id"])
 
 ######################
 # Index, information #
@@ -65,16 +71,26 @@ def settings_page():
 	"""Settings page."""
 	if "user_id" in session:
 		return redirect(url_for('settings_account'))
-	return redirect(url_for('login_page'))
+	return redirect(url_for('auth_login'))
 
-@app.route('/settings/account')
+@app.route('/settings/account', methods=["GET", "POST"])
 def settings_account():
 	"""Account settings."""
+	if request.method == "POST":
+		user_dict = db.get_object_as_dict_by_id(session['user_id'])
+		user_dict['username'] == request.form['username'],
+		user_dict['email'] == validate_email(request.form['email']).email
+		db.push_object(session['user_id'], objects.make_object_from_dict(user_dict, extend=session['user_id']))
+		update_user()
 	if not "user_id" in session:
-		return redirect(url_for('login_page'))
+		return redirect(url_for('auth_login'))
 	instance = db.get_object_as_dict_by_id("0")
+	update_user()
+	user_dict = session['user_dict']
 	return render_template("settings/account.html",
-	                       user_name=db.get_object_as_dict_by_id(session['user_id']),
+	                       user_dict=user_dict,
+	                       user_name=user_dict["username"],
+	                       user_email=user_dict["email"],
 	                       instance_name=instance["name"],
 	                       instance_description=instance["description"],
 	                       instance_domain=instance["address"])
